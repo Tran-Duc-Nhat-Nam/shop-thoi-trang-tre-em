ec68499977bd194dc701b7ca7353f38646fedd07
Tran-Duc-Nhat-Nam <87745020+Tran-Duc-Nhat-Nam@users.noreply.github.com>
2022-05-19 08:01:00 +0700

    Add files via upload

--- signup.js
+++ signup.js
@@ -10,11 +10,11 @@ let dark = false;
 
 function darkMode() {
     let body = document.getElementById('bd');
-    let form = document.getElementById('main');
+    let form = document.getElementById('main_form');
 
     if (dark)
     {
-        body.style.backgroundImage = 'linear-gradient(violet, skyblue)';
+        body.style.backgroundImage = 'linear-gradient(115deg, pink, skyblue)';
         body.style.backgroundColor = 'white';
         form.style.boxShadow = '6px 6px 15px gray';
         dark = false;
